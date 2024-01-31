4246f8b0770d88778843caa3f28e1089d6db1f4b
Shamble94 <fratus.nicholas@gmail.com>
2024-01-31 13:25:23 +0100

    Created class Products and sub-class DogFeed

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <?php
 
+    class Products{
+        public $name;
+        public $price;
+        public $weight;
+    }
+    function __construct($_name, $_price, $_weight, Animal $_type){
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->weight = $_weight;
+        $this->type = $_type
+    }
 
 
 
+    class Animal{
+        public $type;
 
+        function __construct($_type){
+            $this->type = $_type;
+        }
+    }
+
+    class DogFeed extends Products{
+        public $size;
+        public $type;
+
+        function __construct($_name, $_price, $_weight, Animal $_type, $_size, $_type){
+            parent::__construct($_name, $_price, $_weight, Animal $_type);
+            $this->size = $_size;
+            $this->type = $_type
+        }
+    }
 ?>
 
 
