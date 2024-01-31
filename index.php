4f1fec5f868e5d2f9ef9091cf0dfd16fdb0c9754
Shamble94 <fratus.nicholas@gmail.com>
2024-01-31 14:26:12 +0100

    Visualizzate a schermo liste cuccie e cibo

--- index.php
+++ index.php
@@ -6,12 +6,20 @@
         public $type;
         
     
-    function __construct($_name, $_price, Animal $_type){
+    function __construct($_name, $_price, $_type){
         $this->name = $_name;
         $this->price = $_price;
         $this->type = $_type;
         
         
+        }
+        public function getRaceAsString(){
+            $race = [];
+            foreach($this->type as $type){
+                array_push($race, $type->type );
+            }
+            $race_string = implode(" / ", $race);
+            return  $race_string;
         }
     }
     /* CLASSE ANIMALI */
@@ -25,16 +33,15 @@
     /* CLASSE CIBO */
     class Feed extends Products{
         public $size;
-        public $type;
         public $weight;
 
-        function __construct($_name, $_price, $_size, $_weight){
-            parent::__construct($_name, $_price,  $_type);
+        function __construct($_name, $_price, $_type, $_size, $_weight){
+            parent::__construct($_name, $_price, $_type);
             $this->size = $_size;
             $this->weight = $_weight;
         } 
     }
-    class Toy extends Products{
+    class Accessory extends Products{
         public $size;
 
         function __construct($_name, $_price,  $_type, $_size){
@@ -56,18 +63,47 @@
     }
 
 
-    $animal_1 = new Animal ("Cane");
-    $animal_2 = new Animal ("Gatto");
-    $animal_3 = new Animal ("Pesce");
+    $animal_1 = new Animal ("Cani");
+    $animal_2 = new Animal ("Gatti");
+    $animal_3 = new Animal ("Pesci");
+    $animal_4 = new Animal ("Uccelli");
 
+    $cibo_1 = new Feed ("royal canin plus", 10.99, [$animal_1], "Adulto", "750g");
+    $cibo_2 = new Feed ("chip chip", 10.98, [$animal_4], "Qualsiasi", "200g");
 
-    $prodotto_2 = new Products ("royal canin plus", "10.99", $animal_1);
-    var_dump($prodotto_2);
-    $prodotto_1 = new AnimalHouse ("cuccia", 50.99, $animal_1, "50cm x 70cm", "Gomma");
-    var_dump($prodotto_1);
-    $giocattolo_1 = new Toy ("osso che suona al morso", 5.99, $animal_1, "Media");
-    var_dump($giocattolo_1);
-?>
+    $cuccia_1 = new AnimalHouse ("Cuccia comfy", 50.99, [$animal_1, $animal_2], "50cm x 70cm", "Gomma");
+    $cuccia_2 = new AnimalHouse ("Bolla premium", 20.95, [$animal_3], "70cm", "Vetro");
+    $cuccia_3 = new AnimalHouse ("Gabbia Deluxe ", 15.99, [$animal_4], "100cm x 40cm", "Ferro");
+
+    $giocattolo_1 = new Accessory ("osso che suona al morso", 5.99, $animal_1, "Media");
+    $cuccie = [
+        $cuccia_1,
+        $cuccia_2,
+        $cuccia_3,
+
+    ];
+    $feeds = [
+        $cibo_1,
+        $cibo_2,
+    ];
+
+    
+    echo "Lista cuccie";
+    foreach ($cuccie as $cuccia_animali){
+        echo
+        "<ul>" .
+        "<li>". "Nome articolo:". " " .$cuccia_animali->name ."<br>". "Prezzo:". " " .$cuccia_animali->price ."<br>" ."Per che animale è? : ". " " .$cuccia_animali->getRaceAsString() ."<br>" . "Grandezza: " .$cuccia_animali->dimension . "<br>". "Materiale: " .$cuccia_animali->material."</li>" 
+        ."</ul>";
+    }
+
+    echo "Lista cibo";
+    foreach ($feeds as $cibo_animali){
+        echo
+        "<ul>" .
+        "<li>". "Nome articolo:". " " .$cibo_animali->name ."<br>". "Prezzo:". " " .$cibo_animali->price ."<br>" ."Per che animale è? : ". " " .$cibo_animali->getRaceAsString() ."<br>" . "Fascia d'eta a cui somministrare: " .$cibo_animali->size . "<br>". "Peso prodotto: " .$cibo_animali->weight."</li>" 
+        ."</ul>";
+    }
+    ?>
 
 
 
@@ -77,6 +113,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
