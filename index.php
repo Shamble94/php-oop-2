9e37c69ad7dbf64febc22ada74d22c547821597a
Shamble94 <fratus.nicholas@gmail.com>
2024-01-31 13:57:48 +0100

    Creazione sottoclassi e incapsulamento classe Animal

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-
+    /* CLASSE PRODOTTI */
     class Products{
         public $name;
         public $price;
-        public $weight;
-    }
-    function __construct($_name, $_price, $_weight, Animal $_type){
+        public $type;
+        
+    
+    function __construct($_name, $_price, Animal $_type){
         $this->name = $_name;
         $this->price = $_price;
-        $this->weight = $_weight;
-        $this->type = $_type
+        $this->type = $_type;
+        
+        
+        }
     }
-
-
-
+    /* CLASSE ANIMALI */
     class Animal{
         public $type;
 
@@ -21,26 +22,52 @@
             $this->type = $_type;
         }
     }
-
-    class DogFeed extends Products{
+    /* CLASSE CIBO */
+    class Feed extends Products{
         public $size;
         public $type;
+        public $weight;
 
-        function __construct($_name, $_price, $_weight, Animal $_type, $_size, $_type){
-            parent::__construct($_name, $_price, $_weight, Animal $_type);
+        function __construct($_name, $_price, $_size, $_weight){
+            parent::__construct($_name, $_price,  $_type);
             $this->size = $_size;
-            $this->type = $_type
+            $this->weight = $_weight;
+        } 
+    }
+    class Toy extends Products{
+        public $size;
+
+        function __construct($_name, $_price,  $_type, $_size){
+            parent::__construct($_name, $_price, $_type);
+            $this->size = $_size;
+            
+        }
+    }   
+
+    class AnimalHouse extends Products{
+        public $dimension;
+        public $material;
+
+        function __construct($_name, $_price,  $_type, $_dimension, $_material){
+            parent::__construct($_name, $_price, $_type);
+            $this->dimension = $_dimension;
+            $this->material = $_material;
         }
     }
+
+
+    $animal_1 = new Animal ("Cane");
+    $animal_2 = new Animal ("Gatto");
+    $animal_3 = new Animal ("Pesce");
+
+
+    $prodotto_2 = new Products ("royal canin plus", "10.99", $animal_1);
+    var_dump($prodotto_2);
+    $prodotto_1 = new AnimalHouse ("cuccia", 50.99, $animal_1, "50cm x 70cm", "Gomma");
+    var_dump($prodotto_1);
+    $giocattolo_1 = new Toy ("osso che suona al morso", 5.99, $animal_1, "Media");
+    var_dump($giocattolo_1);
 ?>
-
-
-
-
-
-
-
-
 
 
 
