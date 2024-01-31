1f6ccac067855c7c9c59979b9edad41080d53e9c
Shamble94 <fratus.nicholas@gmail.com>
2024-01-31 14:47:08 +0100

    Aggiunto file css e immagini

--- index.php
+++ index.php
@@ -34,10 +34,12 @@
     class Feed extends Products{
         public $size;
         public $weight;
+        public $image;
 
-        function __construct($_name, $_price, $_type, $_size, $_weight){
+        function __construct($_name, $_price, $_type,  $_size, $_image, $_weight){
             parent::__construct($_name, $_price, $_type);
             $this->size = $_size;
+            $this->image = $_image;
             $this->weight = $_weight;
         } 
     }
@@ -68,9 +70,9 @@
     $animal_3 = new Animal ("Pesci");
     $animal_4 = new Animal ("Uccelli");
 
-    $cibo_1 = new Feed ("royal canin plus", 10.99, [$animal_1], "Adulto", "750g");
-    $cibo_2 = new Feed ("chip chip", 10.98, [$animal_4], "Qualsiasi", "200g");
-
+    $cibo_1 = new Feed ("Royal canin mini adult", 10.99, [$animal_1], "Adulto", "<img src=https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000>", "750g");
+    $cibo_2 = new Feed ("chip chip", 10.98, [$animal_4], "Qualsiasi", "<img src=https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg>" ,"200g");
+    
     $cuccia_1 = new AnimalHouse ("Cuccia comfy", 50.99, [$animal_1, $animal_2], "50cm x 70cm", "Gomma");
     $cuccia_2 = new AnimalHouse ("Bolla premium", 20.95, [$animal_3], "70cm", "Vetro");
     $cuccia_3 = new AnimalHouse ("Gabbia Deluxe ", 15.99, [$animal_4], "100cm x 40cm", "Ferro");
@@ -88,7 +90,7 @@
     ];
 
     
-    echo "Lista cuccie";
+/*     echo "Lista cuccie";
     foreach ($cuccie as $cuccia_animali){
         echo
         "<ul>" .
@@ -102,7 +104,7 @@
         "<ul>" .
         "<li>". "Nome articolo:". " " .$cibo_animali->name ."<br>". "Prezzo:". " " .$cibo_animali->price ."<br>" ."Per che animale è? : ". " " .$cibo_animali->getRaceAsString() ."<br>" . "Fascia d'eta a cui somministrare: " .$cibo_animali->size . "<br>". "Peso prodotto: " .$cibo_animali->weight."</li>" 
         ."</ul>";
-    }
+    } */
     ?>
 
 
@@ -113,10 +115,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
-</body>
-</html>
+    </body>
+    <?php  
+    foreach ($feeds as $cibo_animali){
+        echo
+        "<div class=card>" . "Nome articolo:". " " .$cibo_animali->name ."<br>". $cibo_animali->image ."Prezzo:". " " .$cibo_animali->price ."<br>" ."Per che animale è? : ". " " .$cibo_animali->getRaceAsString() ."<br>" . "Fascia d'eta a cui somministrare: " .$cibo_animali->size . "<br>". "Peso prodotto: " .$cibo_animali->weight 
+        ;
+    } ?>
+        </html>
